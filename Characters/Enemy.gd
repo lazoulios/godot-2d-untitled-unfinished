92d80338bff5a32c0fb8e33950335da0adce0a31
lazos <lazgiorgos01@gmail.com>
2024-02-10 23:07:13 +0200

    Updated some bugs. Changed tilesets.

--- Characters/Enemy.gd
+++ Characters/Enemy.gd
@@ -1,19 +1,20 @@
 extends CharacterBody2D
 
-@export var speed : float = 50
+@export var speed : float = 100
+
 var player_chase = false
 var player = null
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if player_chase:
-		position += (player.position -position) / speed
+		position = position + (player.position -position) / speed
 
 
-func _on_actionable_body_entered(body:Node2D):
+func _on_detection_area_body_entered(body:Node2D):
 	player = body
 	player_chase = true
 
 
-func _on_actionable_body_exited(body:Node2D):
+func _on_detection_area_body_exited(_body:Node2D):
 	player = null
-	player_chase = false
+	player_chase = false 
