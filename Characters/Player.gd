2ac4463d144e26878fc3553514f3deb12f885011
lazos <112394398+lazoulios@users.noreply.github.com>
2024-02-11 17:07:05 +0200

    Added swing animation successfuly and removed the gun

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -2,51 +2,92 @@ extends CharacterBody2D
 
 @export var move_speed : float = 100
 @export var sprint_speed : float = 20
-@export var starting_direction : Vector2 = Vector2(0,1)
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-func _ready():
-	update_animation_parameters(starting_direction)
+var input_movement = Vector2.ZERO
+
+enum player_states {MOVE, IDLE, RUN, ATTACK}
+var current_state = player_states.MOVE
 
 func _physics_process(_delta):
-	# Get input direction
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	match current_state:
+		player_states.MOVE:
+			move()
+		player_states.ATTACK:
+			attack()
 
-	var sprint = Input.get_action_strength("run")
-	var attack = Input.get_action_strength("attack")
+func move():
 
-	update_animation_parameters(input_direction)
+	input_movement = Input.get_vector("left","right","up","down")
+	
+	if input_movement != Vector2.ZERO:
+		animation_tree.set("parameters/idle/blend_position",input_movement)
+		animation_tree.set("parameters/walk/blend_position",input_movement)
+		animation_tree.set("parameters/run/blend_position",input_movement)
+		animation_tree.set("parameters/attack/blend_position",input_movement)
+		state_machine.travel("walk")
+		velocity = input_movement * move_speed
 
-	# Update velocity
-	if sprint == 1:
-		velocity = input_direction.normalized() * (move_speed + sprint_speed)
-	else:
-		velocity = input_direction.normalized() * move_speed
+	if input_movement == Vector2.ZERO:
+		state_machine.travel("idle")
+		velocity = Vector2.ZERO
 
-	# Move and Slide func ises velocity of character body to move character on map
+	if Input.is_action_just_pressed("attack"):
+		current_state = player_states.ATTACK
+
 	move_and_slide()
 
-	pick_new_state(attack, sprint)
+func attack():
+	state_machine.travel("attack")
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * move_speed
+	move_and_slide()
 
-func update_animation_parameters(move_input : Vector2):
-	# Don't change animation parameters if there is no move input
-	if (move_input != Vector2.ZERO):
-		animation_tree.set("parameters/idle/blend_position",move_input)
-		animation_tree.set("parameters/walk/blend_position",move_input)
-		animation_tree.set("parameters/run/blend_position",move_input)
-		animation_tree.set("parameters/attack/blend_position",move_input)
+func on_state_reset():
+	current_state = player_states.MOVE
+
+# func _ready():
+# 	update_animation_parameters(starting_direction)
+
+# func _physics_process(_delta):
+# 	# Get input direction
+# 	var input_direction = Vector2(
+# 		Input.get_action_strength("right") - Input.get_action_strength("left"),
+# 		Input.get_action_strength("down") - Input.get_action_strength("up")
+# 	)
+
+# 	var sprint = Input.get_action_strength("run")
+
+# 	if Input.get_action_strength("attack"):
+# 		current_state = player_states.ATTACK
+
+# 	update_animation_parameters(input_direction)
+
+# 	# Update velocity
+# 	if sprint == 1:
+# 		velocity = input_direction.normalized() * (move_speed + sprint_speed)
+# 	else:
+# 		velocity = input_direction.normalized() * move_speed
+
+# 	# Move and Slide func ises velocity of character body to move character on map
+# 	move_and_slide()
+
+# 	pick_new_state(sprint)
+
+# func update_animation_parameters(move_input : Vector2):
+# 	# Don't change animation parameters if there is no move input
+# 	if (move_input != Vector2.ZERO):
+# 		animation_tree.set("parameters/idle/blend_position",move_input)
+# 		animation_tree.set("parameters/walk/blend_position",move_input)
+# 		animation_tree.set("parameters/run/blend_position",move_input)
+# 		animation_tree.set("parameters/attack/blend_position",move_input)
 		
-func pick_new_state(attack, sprint):
-	if (attack==1):
-		state_machine.travel("attack")
-	elif (velocity!=Vector2.ZERO && sprint!=1):
-		state_machine.travel("walk")
-	elif (velocity!=Vector2.ZERO && sprint==1):
-		state_machine.travel("run")
-	else:
-		state_machine.travel("idle")
+# func pick_new_state(sprint):
+# 	if (velocity!=Vector2.ZERO && sprint!=1):
+# 		state_machine.travel("walk")
+# 	elif (velocity!=Vector2.ZERO && sprint==1):
+# 		state_machine.travel("run")
+# 	else:
+# 		state_machine.travel("idle")
