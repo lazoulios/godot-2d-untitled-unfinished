666a58d1182eaa3c95e95c10a5c66569c612f43e
lazos <112394398+lazoulios@users.noreply.github.com>
2024-02-14 19:05:26 +0200

    Added hit animation for player

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -8,8 +8,8 @@ extends CharacterBody2D
 
 var input_movement = Vector2.ZERO
 
-enum player_states {MOVE, IDLE, RUN, ATTACK}
-var current_state = player_states.MOVE
+enum player_states {WALK, IDLE, RUN, ATTACK, HIT}
+var current_state = player_states.WALK
 
 var enemy_in_attack_range = false
 var enemy_attack_cooldown = true
@@ -19,10 +19,17 @@ var player_alive = true
 func _physics_process(_delta):
 	enemy_attack()
 	match current_state:
-		player_states.MOVE:
+		player_states.WALK:
 			move()
 		player_states.ATTACK:
 			attack()
+		player_states.IDLE:
+			move()
+		player_states.RUN:
+			move()
+		player_states.HIT:
+			hit()
+	
 	if health <=0:
 		player_alive = false #go to respawn point
 		health = 0
@@ -39,14 +46,17 @@ func move():
 		if Input.get_action_strength("run"):
 			velocity = input_movement * (move_speed+sprint_speed)
 			state_machine.travel("run")
+			current_state = player_states.RUN
 		else :
 			velocity = input_movement * move_speed
 			state_machine.travel("walk")
+			current_state = player_states.WALK
 			
 
 	if input_movement == Vector2.ZERO:
 		state_machine.travel("idle")
 		velocity = Vector2.ZERO
+		current_state = player_states.IDLE
 
 	if Input.is_action_just_pressed("attack"):
 		current_state = player_states.ATTACK
@@ -60,7 +70,7 @@ func attack():
 	move_and_slide()
 
 func on_state_reset():
-	current_state = player_states.MOVE
+	current_state = player_states.WALK
 
 func _on_player_hitbox_body_exited(body:Node2D):
 	if body.has_method("enemy"):
@@ -73,11 +83,14 @@ func _on_player_hitbox_body_entered(body:Node2D):
 func enemy_attack():
 	if enemy_in_attack_range==true and enemy_attack_cooldown==true:
 		health -= 10
-		state_machine["parameters/conditions/hit"] = true
+		current_state = player_states.HIT
 		print(health)
 		enemy_attack_cooldown = false
 		$Timer.start()
 
 func _on_timer_timeout():
 	enemy_attack_cooldown = true
-	state_machine["parameters/conditions/hit"] = false
+
+func hit():
+	state_machine.travel("hit")
+	current_state = player_states.WALK
